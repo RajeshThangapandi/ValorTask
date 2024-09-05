8beea5de2800ffd6da2a7f105bedb5d789e283f0
RajeshThangapandi <126014783+RajeshThangapandi@users.noreply.github.com>
2024-09-05 14:09:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+// app.js
 const express = require('express');
-const app = express();
 const connectDB = require('./config/db');
+const swaggerDocs = require('./swagger/swagger');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
 const internRoutes = require('./routes/internRoutes');
 const performanceRoutes = require('./routes/performanceRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const feedbackRoutes = require('./routes/feedbackRoutes');
 const authRoutes = require('./routes/authRoutes');
 
-// Connect to MongoDB
-connectDB();
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Use routes
-app.use('/api/intern', internRoutes);
+app.use('/api/interns', internRoutes);
 app.use('/api/performance', performanceRoutes);
-app.use('/api/task', taskRoutes);
+app.use('/api/tasks', taskRoutes);
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/auth', authRoutes);
 
+// Swagger docs
+swaggerDocs(app);
+
+// Start server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
