0fe7f60e2dc259709058ff44a107097deb4f8073
RajeshThangapandi <126014783+RajeshThangapandi@users.noreply.github.com>
2024-09-05 13:58:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,13 @@
+// config/db.js
 const mongoose = require('mongoose');
-require('dotenv').config(); // Make sure to require dotenv to use .env variables
 
 const connectDB = async () => {
     try {
-        const dbUri = process.env.MONGODB_URI; // Retrieve the URI from environment variables
-        if (!dbUri) {
-            throw new Error('MongoDB URI is not defined');
-        }
-        await mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI); // Remove useNewUrlParser and useUnifiedTopology options
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
-        process.exit(1); // Exit the process with failure code
+        process.exit(1);
     }
 };
 
